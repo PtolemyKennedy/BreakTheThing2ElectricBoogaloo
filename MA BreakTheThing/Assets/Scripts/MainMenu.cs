e9fe1ac941a6dc699f5c3502c6ffb049bae845e9
supermatt278 <super_matt@live.ie>
2017-11-21 14:52:20 +0000

    Mainmenuorsomething
    +added main menu

---
+++ MA BreakTheThing/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject levelButtonPrefab;
+    public GameObject levelButtonContainer;
+
+    private void Start()
+    {
+        Sprite[] thumbnails = Resources.LoadAll<Sprite>("Levels"); //Creates array of levels in folder
+        foreach (Sprite thumbnail in thumbnails)
+        {
+            GameObject container = Instantiate(levelButtonPrefab) as GameObject;
+            container.GetComponent<Image>().sprite = thumbnail;
+            container.transform.SetParent(levelButtonContainer.transform,false); //false here sets the location to the container
+
+            string sceneName = thumbnail.name;
+            container.GetComponent<Button>().onClick.AddListener(() => LoadLevel(sceneName)); //called every time a button is clicked and changes depending on button
+        }
+    }
+    private void LoadLevel (string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LookAtMenu(Transform menuTransform)
+    {
+        Camera.main.transform.LookAt(menuTransform.position);
+    }
+}
