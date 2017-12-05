3c72be37570f82f65e5a27709cbf98df2804df5e
EdBurt2 <2burte51@solent.ac.uk>
2017-12-05 14:09:04 +0000

    failes

--- MA BreakTheThing/Assets/Scripts/MainMenu.cs
+++ MA BreakTheThing/Assets/Scripts/MainMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using GoogleMobileAds.Api;
 
 public class MainMenu : MonoBehaviour {
 
@@ -23,6 +24,42 @@ public class MainMenu : MonoBehaviour {
             container.GetComponent<Button>().onClick.AddListener(() => LoadLevel(sceneName)); //called every time a button is clicked and changes depending on button
         }
     }
+    private void RequestBanner()
+    {
+#if UNITY_ANDROID
+        string adUnitId = "ca-app-pub-7348650003716399/4455741290";
+#else
+        string adUnitId = "unexpected_platform";
+#endif
+
+        // Create a 320x50 banner at the top of the screen.
+        BannerView bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder()
+     .AddTestDevice(AdRequest.TestDeviceSimulator)       // Simulator.
+     .AddTestDevice("2077ef9a63d2b398840261c8221a0c9b")  // My test device.
+     .Build();
+
+        // Load the banner with the request.
+        bannerView.LoadAd(request);
+    }
+    private void RequestInterstitial()
+    {
+#if UNITY_ANDROID
+        string adUnitId = "ca-app-pub-7348650003716399/8471007928";
+#else
+        string adUnitId = "unexpected_platform";
+#endif
+
+        // Initialize an InterstitialAd.
+        InterstitialAd interstitial = new InterstitialAd(adUnitId);
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the interstitial with the request.
+        interstitial.LoadAd(request);
+    }
+
+
     private void LoadLevel (string sceneName)
     {
         SceneManager.LoadScene(sceneName);
@@ -31,5 +68,9 @@ public class MainMenu : MonoBehaviour {
     public void LookAtMenu(Transform menuTransform)
     {
         Camera.main.transform.LookAt(menuTransform.position);
+    }
+    private void Update()
+    {
+        
     }
 }
