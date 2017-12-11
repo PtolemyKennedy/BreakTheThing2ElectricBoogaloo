d87b5faa4c4b2d4d7f2a57ec9058b405792ad6e2
PtolemyKennedy <ptolemy96@yahoo.co.uk>
2017-12-11 11:50:28 +0000

    added code that doesnt work

--- MA BreakTheThing/Assets/Scripts/JointBreak.cs
+++ MA BreakTheThing/Assets/Scripts/JointBreak.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class JointBreak : MonoBehaviour
 {
@@ -24,6 +25,30 @@ public class JointBreak : MonoBehaviour
     private void OnJointBreak(float breakForce)
     {
         _ps.ScorePoints(fj.breakForce);
-
+        //SpawnPoints();
     }
+
+    //attempt at making points pop up on screen
+    
+    //private void SpawnPoints()
+    //{       
+    //    //instantiate pop up of points scored
+    //    GameObject PopUp = Instantiate(Resources.Load("PointsPopUp"), gameObject.transform) as GameObject;
+    //    PopUp.transform.localPosition = transform.position;
+    //    PopUp.transform.localScale = new Vector3(1, 1, 1);
+    //    PopUp.transform.GetComponent<Text>().text = "+ " + fj.breakForce;
+
+    //    StartCoroutine(MoveUpAndFade(PopUp));
+    //}
+
+    //IEnumerator MoveUpAndFade(GameObject go)
+    //{
+    //    //makes the gameobject move up and fade out over 2 seconds
+    //    for (float i = 1f; i >= 0; i -= 0.05f)
+    //    {
+    //        go.transform.Translate(0, 5, 0);
+    //        yield return new WaitForSeconds(0.05f);
+    //    }
+    //    Destroy(go);
+    //}
 }
