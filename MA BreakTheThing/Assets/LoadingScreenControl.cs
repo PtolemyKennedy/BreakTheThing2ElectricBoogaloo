e08bb2ab79569411333f5f4058bb1029b6e93c31
supermatt278 <super_matt@live.ie>
2018-01-03 00:10:19 +0000

    Update 3
    +Optimized particle effects
    +Added more info to menu
    +Added loading screen

---
+++ MA BreakTheThing/Assets/LoadingScreenControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreenControl : MonoBehaviour
+{
+    public GameObject loadingScreenObj;
+    public Slider slider;
+
+    AsyncOperation async; //
+
+    public void LoadScreenExample()
+    {
+        StartCoroutine(LoadingScreen());
+    }
+
+    IEnumerator LoadingScreen()
+    {
+        loadingScreenObj.SetActive(true);
+        async = SceneManager.LoadSceneAsync(0);
+        async.allowSceneActivation = false;
+
+        while (async.isDone == false)
+        {
+            slider.value = async.progress;
+            if (async.progress == 0.9f)
+            {
+                slider.value = 1f;
+                async.allowSceneActivation = true;
+            }
+            yield return null;
+
+        }
+    }
+}
